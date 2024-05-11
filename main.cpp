add668e3e99f9dd0f9fe414740823040ccde4a5b
w25m <mrakiren@gmail.com>
2024-05-11 12:58:13 +0200

    Sin i cos

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <matplot/matplot.h>
 #include <iostream>
 #define _USE_MATH_DEFINES
-#include <math.h> //1234
+#include <math.h>
 using namespace std;
-void generateSinusoidalSignal(double frequency, double duration) {
+void gensin(double frequency, double duration) {
     const double samplingRate = 10000.0; // więcej tego bardziej dokładny wykres (nie wiesz daj na 10 to ogarniesz o co chodzi)
     const int numSamples = static_cast<int>(duration * samplingRate);
 
@@ -28,13 +28,53 @@ void generateSinusoidalSignal(double frequency, double duration) {
     matplot::show();
 }
 
+
+void gencos(double frequency, double duration) {
+    const double samplingRate = 10000.0; // więcej tego bardziej dokładny wykres (nie wiesz daj na 10 to ogarniesz o co chodzi)
+    const int numSamples = static_cast<int>(duration * samplingRate);
+
+    vector<double> timeValues;
+    vector<double> signalValues;
+
+    for (int i = 0; i < numSamples; ++i) { // tworzy punkty na podstawie których wykres
+        double t = static_cast<double>(i) / samplingRate;
+        double amplitude = cos(2.0 * M_PI * frequency * t);
+
+        timeValues.push_back(t);
+        signalValues.push_back(amplitude);
+    }
+
+    //Tworzy wykres
+    matplot::plot(timeValues, signalValues);
+    matplot::title("Sygnał");
+    matplot::xlabel("Czas [s]");
+    matplot::ylabel("Amplituda");
+
+
+    matplot::show();
+}
+
 int main() {
-    
-    double desiredFrequency = 4.0; // ile sinusów na sekunde
-    double signalDuration = 2.0; // czas oś X
-
-    generateSinusoidalSignal(desiredFrequency, signalDuration);
-
+    double frequency = 2.0; // ile sinusów na sekunde
+    double time = 2.0;
+    int value;
+    printf("Podaj czestotliwosc:"); 
+    cin >> frequency;
+    printf("Podaj czas sygnalu:");
+    cin >> time;
+    printf("Podaj co chcesz wyswietlic sin[1], cos[2]:");
+    cin >> value;
+    switch (value)
+    {
+    case 1:
+        gensin(frequency, time);
+        break;
+    case 2:
+        gencos(frequency, time);
+        break;
+    default:
+        break;
+    }
     return 0;
 }
 	
