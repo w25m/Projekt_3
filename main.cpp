7675837d447eeebc817a04c968c248e67cfa9389
JozMroz <praca.jozefmroZ@GMAIL.COM>
2024-05-11 13:43:02 +0200

    rec&saw.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 using namespace std;
+
+int sgn(double x) {
+    return x>=0 ? 1 : -1;
+}
+
 void gensin(double frequency, double duration) {
     const double samplingRate = 10000.0; // więcej tego bardziej dokładny wykres (nie wiesz daj na 10 to ogarniesz o co chodzi)
     const int numSamples = static_cast<int>(duration * samplingRate);
@@ -24,7 +29,7 @@ void gensin(double frequency, double duration) {
     matplot::xlabel("Czas [s]");
     matplot::ylabel("Amplituda");
 
-  
+
     matplot::show();
 }
 
@@ -54,15 +59,69 @@ void gencos(double frequency, double duration) {
     matplot::show();
 }
 
+void genrec(double frequency, double duration) {
+    const double samplingRate = 10000.0; // więcej tego bardziej dokładny wykres (nie wiesz daj na 10 to ogarniesz o co chodzi)
+    const int numSamples = static_cast<int>(duration * samplingRate);
+
+    vector<double> timeValues;
+    vector<double> signalValues;
+
+    for (int i = 0; i < numSamples; ++i) { // tworzy punkty na podstawie których wykres
+        double t = static_cast<double>(i) / samplingRate;
+        double sinValue = sin(2.0 * M_PI * frequency * t);
+        double amplitude = (sinValue <= 0) ? -1 : 1;
+        
+        timeValues.push_back(t);
+        signalValues.push_back(amplitude);
+    }
+
+    //Tworzy wykres
+    matplot::plot(timeValues, signalValues);
+    matplot::title("Sygnał");
+    matplot::xlabel("Czas [s]");
+    matplot::ylabel("Amplituda");
+    matplot::xlim({ 0, duration });
+    matplot::ylim({ -1.5, 1.5 });
+
+    matplot::show();
+}
+
+void gensaw(double frequency, double duration) {
+    const double samplingRate = 10000.0; // więcej tego bardziej dokładny wykres (nie wiesz daj na 10 to ogarniesz o co chodzi)
+    const int numSamples = static_cast<int>(duration * samplingRate);
+
+    vector<double> timeValues;
+    vector<double> signalValues;
+
+    for (int i = 0; i < numSamples; ++i) { // tworzy punkty na podstawie których wykres
+        double t = static_cast<double>(i) / samplingRate;
+        double amplitude = 2.0 * fmod(t * frequency, 1.0) - 1.0;
+        
+
+        timeValues.push_back(t);
+        signalValues.push_back(amplitude);
+    }
+
+    //Tworzy wykres
+    matplot::plot(timeValues, signalValues);
+    matplot::title("Sygnał");
+    matplot::xlabel("Czas [s]");
+    matplot::ylabel("Amplituda");
+    matplot::xlim({ 0, duration });
+    matplot::ylim({ -1.5, 1.5 });
+
+    matplot::show();
+}
+
 int main() {
     double frequency = 2.0; // ile sinusów na sekunde
     double time = 2.0;
     int value;
-    printf("Podaj czestotliwosc:"); 
+    printf("Podaj czestotliwosc:");
     cin >> frequency;
     printf("Podaj czas sygnalu:");
     cin >> time;
-    printf("Podaj co chcesz wyswietlic sin[1], cos[2]:");
+    printf("Podaj co chcesz wyswietlic sin[1], cos[2], rec[3], saw[4]: ");
     cin >> value;
     switch (value)
     {
@@ -72,10 +131,14 @@ int main() {
     case 2:
         gencos(frequency, time);
         break;
+    case 3:
+        genrec(frequency, time);
+        break;
+    case 4:
+        gensaw(frequency, time);
+        break;
     default:
         break;
     }
     return 0;
 }
-	
-	
